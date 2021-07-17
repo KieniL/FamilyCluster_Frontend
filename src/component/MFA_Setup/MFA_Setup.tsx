f7c7752549217d9ad78d977438dca2cd69bb6f57
Lukas Kienast <lukaskienast0@gmail.com>
2021-07-17 13:49:01 +0200

    corrected path to mfa verify

--- src/component/MFA_Setup/MFA_Setup.tsx
+++ src/component/MFA_Setup/MFA_Setup.tsx
@@ -14,7 +14,7 @@ class MFA_Setup extends React.Component {
                 <p>Download from <a href="https://itunes.apple.com/us/app/freeotp/id872559395">iTunes</a> | <a href="https://play.google.com/store/apps/details?id=org.fedorahosted.freeotp">Google Play</a></p>
                 <p>
                 { localStorage.getItem('mfaimage') != null? <img src={`data:image/png;base64,${ localStorage.getItem('mfaimage')}`} alt="mfaQR"/>: ''}</p>
-                    <p>I'm done, take me to  <a href="/mfa/verify">Verify</a> page!</p>
+                    <p>I'm done, take me to  <a href="/frontend/mfa/verify">Verify</a> page!</p>
             </div>
         );
     }
